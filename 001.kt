f13e6531c1c57ae7c15b66a3e99cc18f112fcf6b
Guito-o <50958532+Guito-o@users.noreply.github.com>
2021-04-05 20:09:05 +0900

    Update 001.kt

--- 001.kt
+++ 001.kt
@@ -1,6 +1,12 @@
+import java.util.Random
+
 fun main() {
     var a = 0
+
     while (a < 31) {
+        var count = 0
+        val random = Random()
+        var rnum = random.nextInt(3)
         var num = readLine()!!.toInt()
 
         if (num > 3)
@@ -11,10 +17,43 @@ fun main() {
         for (i in 1..num) {
             if (a <= 31)
                 a += 1
-                print("$a ")
+            print("$a ")
         }
         println()
+        if (a == 30){
+            println("My Win")
+            break
+        }
+        println("--")
 
-        for (i in )
+        for (j in 1..3) {
+            if ((a + j - 2) % 4 == 0) {
+                println("CPU : $j")
+                for (i in 1..j) {
+                    if (a <= 31)
+                    a += 1
+                    print("$a ")
+                }
+                println()
+                if (a == 30) {
+                    println("CPU Win")
+                }
+                break
+            }
+            count += 1
+        }
+        if (count == 3) {
+            rnum += 1
+            println("CPU : $rnum")
+            for (i in 1..rnum) {
+                a += 1
+                print("$a ")
+            }
+            println()
+            if (a == 30) {
+                println("CPU Win")
+            }
+        }
+        println("--")
     }
 }
